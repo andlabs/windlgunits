81b5a2103463f808b8d7eeda2a599baea033dcb8
Pietro Gagliardi <pietro10@mac.com>
2014-07-31 12:26:06 -0400

    Set up the GUI.

--- main.c
+++ main.c
@@ -8,6 +8,13 @@ INT_PTR CALLBACK mainwinDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 
 int main(void)
 {
+	INITCOMMONCONTROLSEX icc;
+
+	ZeroMemory(&icc, sizeof (INITCOMMONCONTROLSEX));
+	icc.dwSize = sizeof (INITCOMMONCONTROLSEX);
+	icc.dwICC = ICC_LISTVIEW_CLASSES;
+	if (InitCommonControlsEx(&icc) == FALSE)
+		printf("error TODO\n");
 	if (DialogBox(NULL, MAKEINTRESOURCE(rcMainWin), NULL, mainwinDlgProc) != 1)
-		printf("error\n");
+		printf("error TODO\n");
 }
--- winapi.h
+++ winapi.h
@@ -11,3 +11,17 @@
 #define NTDDI_VERSION 0x05010000	/* according to Microsoft's sdkddkver.h */
 #include <windows.h>
 #include <commctrl.h>
+
+// TODO split to a separate file?
+// constants for preferred control sizes and positions
+// via http://msdn.microsoft.com/en-us/library/windows/desktop/dn742486.aspx#sizingspacing
+#define dialogMargin 7
+#define buttonWidth 67		/* originally 50; not wide enough */
+#define buttonHeight 14
+#define buttonXSpace 4
+#define editHeight 14
+#define labelHeight 8
+#define labelYOff 3
+#define labelXSpace 2
+#define horizontalSpace 7
+#define verticalSpace 4
